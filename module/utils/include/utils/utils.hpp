0c2ec054e63f51eb425ea010176214cf80d1965a
Jeong0806 <Jeong0806@github.com>
2023-12-20 10:10:32 +0900

    Added hpp and cpp files for utils

---
+++ module/utils/include/utils/utils.hpp
@@ -0,0 +1,8 @@
+#ifndef UTILS_HPP_
+#define UTILS_HPP_
+#include <string>
+
+void Print(const std::string& message);
+
+#endif
+
---
+++ module/utils/src/utils.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+#include "utils/utils.hpp"
+
+void Print(const std::string& message) {
+  std::cout << message << std::endl;
+}
